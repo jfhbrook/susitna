1e4f37188bbdfc895478c5f45278ce5b690a7a19
Josh Holbrook <josh.holbrook@gmail.com>
2023-12-01 12:23:14 -0900

    visitor pattern in dim file

--- src/dim.rs
+++ src/dim.rs
@@ -3,142 +3,110 @@
 
 use anyhow::{ Result, Error };
 
-pub enum Value {
+pub enum Index<'a> {
+    Number(i64),
+    Key(&'a String)
+    // TODO: various ranges
+}
+
+pub struct Array {}
+
+pub enum Value<'a> {
     Number(i64),
     Real(f64),
-    String(String)
-    // Reference(i64)
+    String(&'a String),
+    // Array(&'a Array)
 }
 
-pub enum Scalar {
-    Number(i64),
-    Real(f64),
-    String(String)
+pub trait Callable {
+    fn call(&self, args: Vec<&Value>) -> Result<Value, Error>;
 }
 
-pub enum Index {
-    Simple(i64),
-    Key(String),
-    Tensor(Vec<i64>)
+// TODO: defaults which return Exceptions
+// TODO: visitors for Arrays
+// TODO: &self ??
+pub trait BinaryOperator {
+    fn num_num(&self, a: i64, b: i64) -> Result<Value, Error>;
+    fn num_real(&self, a: i64, b: f64) -> Result<Value, Error>;
+    fn num_string(&self, a: i64, b: &String) -> Result<Value, Error>;
+
+    fn real_num(&self, a: f64, b: i64) -> Result<Value, Error>;
+    fn real_real(&self, a: f64, b: f64) -> Result<Value, Error>;
+    fn real_string(&self, a: f64, b: &String) -> Result<Value, Error>;
+
+    fn string_num(&self, a: &String, b: i64) -> Result<Value, Error>;
+    fn string_real(&self, a: &String, b: f64) -> Result<Value, Error>;
+    fn string_string(&self, a: &String, b: &String) -> Result<Value, Error>;
 }
 
-pub struct Slice {}
-
-pub struct Callable {}
-
-pub struct Dim {
-}
-
-// I'd actually like this to be more dynamic, but there are a LOT of challenges
-// when it comes to object safety and dyn traits:
-//
-// https://doc.rust-lang.org/reference/items/traits.html#object-safety
-impl Dim {
-    // base operations
-    fn new(dimensions: Vec<i64>) -> Result<Dim, Error> {
-        Ok(Dim {})
-    }
-
-    fn get(&self, i: Index) -> Result<i64, Error> {
-        unimplemented!("get");
-    }
-
-    fn set(&self, i: Index, value: Value) -> Result<(), Error> {
-        unimplemented!("set");
-    }
-
-    // length is total number of elements, both indexes and keys
-    fn length(&self) -> Result<i64, Error> {
-        unimplemented!("length");
-    }
-
-    // size is m x n x ... for numerical keys only
-    fn size(&self) -> Result<Dim, Error> {
-        unimplemented!("size");
-    }
-
-    fn slice(&self, slice: Slice) -> Result<(), Error> {
-        unimplemented!("slice");
-    }
-
-    // scalar operations
-    fn add_scalar(&self, scalar: Scalar) -> Result<Dim, Error> {
-        unimplemented!("add_scalar_to");
-    }
-
-    fn sub_scalar_from(&self, scalar: Scalar) -> Result<Dim, Error> {
-        unimplemented!("sub_scalar_from");
-    }
-
-    fn sub_from_scalar(&self, scalar: Scalar) -> Result<Dim, Error> {
-        unimplemented!("sub_from_scalar");
-    }
-
-    fn mul_scalar(&self, scalar: Scalar) -> Result<Dim, Error> {
-        unimplemented!("mul_scalar");
-    }
-
-    fn div_by_scalar(&self, scalar: Scalar) -> Result<Dim, Error> {
-        unimplemented!("div_by_scalar");
-    }
-
-    fn div_scalar_by(&self, scalar: Scalar) -> Result<Dim, Error> {
-        unimplemented!("div_scalar_by");
-    }
-
-    fn neg(&self) -> Result<dim, Error> {
-        unimplemented!("neg");
-    }
-
-    // element-wise operations
-
-    // tensor operations
-
-    // iterative operations
-    fn keys(&self) -> Result<Dim, Error> {
-        unimplemented!("keys");
-    }
-
-    fn values(&self) -> Result<Dim, Error> {
-        unimplemented!("values");
-    }
-
-    fn entries(&self) -> Result<Dim, Error> {
-        unimplemented!("entries");
-    }
-
-    // function operations
-    fn map(&self, callable: Callable) -> Result<Dim, Error> {
-        unimplemented!("map");
-    }
-
-    fn filter(&self, callable: Callable) -> Result<Dim, Error> {
-        unimplemented!("filter");
-    }
-
-    fn reduce(&self, acc: Value, callable: Callable) -> Result<Value, Error> {
-        unimplemented!("reduce");
-    }
-
-    // stack operations
-    fn push(&self, value: Value) -> Result<(), Error> {
-        unimplemented!("push");
-    }
-
-    fn pop(&self) -> Result<Value, Error> {
-        unimplemented!("pop");
-    }
-
-    fn unshift(&self, value: Value) -> Result<(), Error> {
-        unimplemented!("unshift");
-    }
-
-    fn shift(&self) -> Result<Value, Error> {
-        unimplemented!("shift");
-    }
-
-    // object-oriented operations
-    fn extend(&self, prototype: Dim) -> Result<(), Error> {
-        unimplemented!("extend");
+impl Callable for dyn BinaryOperator {
+    fn call(&self, args: Vec<&Value>) -> Result<Value, Error> {
+        match (args[0], args[1]) {
+            (Value::Number(a), Value::Number(b)) => self.num_num(*a, *b),
+            (Value::Number(a), Value::Real(b)) => self.num_real(*a, *b),
+            (Value::Number(a), Value::String(b)) => self.num_string(*a, b),
+            (Value::Real(a), Value::Number(b)) => self.real_num(*a, *b),
+            (Value::Real(a), Value::Real(b)) => self.real_real(*a, *b),
+            (Value::Real(a), Value::String(b)) => self.real_string(*a, b),
+            (Value::String(a), Value::Number(b)) => self.string_num(a, *b),
+            (Value::String(a), Value::Real(b)) => self.string_real(a, *b),
+            (Value::String(a), Value::String(b)) => self.string_string(a, b)
+        }
     }
 }
+
+pub trait UnaryOperator {
+    fn num(&self, a: i64) -> Result<Value, Error>;
+    fn real(&self, a: f64) -> Result<Value, Error>;
+    fn string(&self, a: &String) -> Result<Value, Error>;
+}
+
+impl Callable for dyn UnaryOperator {
+    fn call(&self, args: Vec<&Value>) -> Result<Value, Error> {
+        match args[9] {
+            Value::Number(a) => self.num(*a),
+            Value::Real(a) => self.real(*a),
+            Value::String(a) => self.string(a)
+        }
+    }
+}
+
+// TODO: should these be straightforward structs/impls?
+pub trait Get {
+    fn string(&self, i: Index) -> Result<Value, Error>;
+}
+
+pub trait Set {
+    fn string(&self, i: Index, v: &String) -> Result<Value, Error>;
+}
+
+/* base ops:
+ * - new(mn, n)
+ * - get
+ * - set
+ * - length
+ * - size
+ * - slice
+ *
+ * associative ops:
+ * - get (key)
+ * - set (key)
+ * element-wise ops
+ * linear ops
+ * iterative ops:
+ * - keys
+ * - values
+ * - entries
+ * functional ops:
+ * - map
+ * - filter
+ * - reduce
+ * sort
+ * stack ops
+ * - push
+ * - pop
+ * - unshift
+ * - shift
+ * oop
+ * - extend
+ */

6d4e49ae12ccc39f85a7a3af0a4e2d085d066dbf
Josh Holbrook <josh.holbrook@gmail.com>
2023-12-28 18:57:37 -0900

    note on interrupts

--- src/main.ts
+++ src/main.ts
@@ -34,6 +34,10 @@ export async function main() {
   // Various things will be able to interrupt the interpreter. The main use
   // case I have in mind is breaking into debug mode, but it will probably
   // be generalizable to event handling in general (with the ON keyword)
+  //
+  // I haven't figured out how this abstraction works yet. It might actually
+  // need a handle on the runtime and other components, instead of those
+  // components getting handles to it - since an interrupt handler is global.
   const interrupts = new Interrupts();
 
   // Runtime runs programs. This is where the program counter lives. Unlike

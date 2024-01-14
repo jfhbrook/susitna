f19182e994b4c15298a387f8b207a0e5db416275
Josh Holbrook <josh.holbrook@gmail.com>
2024-01-14 12:12:39 -0900

    Improve Logger comments

--- src/logger.ts
+++ src/logger.ts
@@ -2,47 +2,42 @@ import { LoggerService } from '@nestjs/common';
 
 import { pino, BaseLogger } from 'pino';
 
-// I don't really like the standard NestJS logger, so I'm rolling my own on
-// top of pino.
+// A logger. For now, it uses Pino.
 //
-// This introduces some challenges, since the API of the NestJS ConsoleLogger
-// makes different assumptions than the standard Pino logger. Things they
-// agree on:
+// This logger mostly exists because NestJS's container expects a logger.
+// However, it may be useful for debug tracing of the interpreter's inner
+// workings.
 //
-// - The first argument may either be an object or a message.
-//
-// Things ConsoleLogger does that pino doesn't:
-//
-// - The last arg is typically treated as a "context" parameter
-// - If exactly two arguments, the second *may* be treated as a serialized
-//   stack
-// - Messages may be arrays - these are serialized the same as objects.
-// - Messages may also be functions or classes. Functions are called at the
-//   time of logging; classes log their names.
-//
-// Things pino does that ConsoleLogger doesn't:
-//
-// - Objects as the first argument are mixed into the JSON output instead of
-//   logged directly
-// - Remaining arguments are treated as formatting parameters.
-//
-// For now, I'm going to naively log things with pino, but if Nest assumes
-// things when logging internally, I may change my mind.
+// All command IO should go through the Host object, not the Logger. However,
+// the Host may use the Logger internally - I haven't decided yet.
+
+// TODO: I need to implement the LoggerService API for NestJS, but it's not
+// necessarily the API that I would want to use internally. If I want to use
+// this logger elsewhere, I should inject it as a Logger and then call
+// app.get/app.use to make NestJS use that logger internally.
+
+// TODO: OpenTelemetry-style tracing support would be *extremely* useful.
+// That's a big project, though. The easist way to support it may be to
+// implement logging/tracing in rust with the tracing crate - yikes.
 
 export class Logger implements LoggerService {
   private readonly logger: BaseLogger;
 
   constructor() {
-    // TODO: Make this configurable on construction. We almost definitely
-    // don't want tracing on by default!
     this.logger = pino({
+      // TODO: Trace should not be the default log level - this should be
+      // enabled by a flag.
       level: 'trace',
       transport: {
+        // TODO: Pino is supposed to be used as a JSON logger. Pretty logs
+        // are convenient to me *now*, but going towards JSON may be
+        // preferred.
         target: 'pino-pretty',
       },
     });
   }
 
+  // NestJS uses "log" internally, so we use this as the "trace" log level.
   log(message: any, ...optionalParams: any[]) {
     this.logger.trace(message, ...optionalParams);
   }
@@ -50,15 +45,24 @@ export class Logger implements LoggerService {
   fatal(message: any, ...optionalParams: any[]) {
     this.logger.fatal(message, ...optionalParams);
   }
+
   error(message: any, ...optionalParams: any[]) {
     this.logger.error(message, ...optionalParams);
   }
+
   warn(message: any, ...optionalParams: any[]) {
     this.logger.warn(message, ...optionalParams);
   }
+
+  // These two methods are optional for NestJS's LoggerService. Go figure.
+
   debug(message: any, ...optionalParams: any[]) {
     this.logger.debug(message, ...optionalParams);
   }
+
+  // TODO: I feel OK about treating "log" as the internal NestJS hook, but
+  // this info/verbose mismatch makes me wonder if I should define custom
+  // log levels. Let's see what shakes out when I implement Host logging.
   verbose(message: any, ...optionalParams: any[]) {
     this.logger.info(message, ...optionalParams);
   }

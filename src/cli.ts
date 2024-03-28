9e2f4778f0f344dffed51d07e15e73594002c601
Josh Holbrook <josh.holbrook@gmail.com>
2024-03-27 16:38:56 -0800

    Documentation for the cli module

--- src/cli.ts
+++ src/cli.ts
@@ -3,21 +3,73 @@ import { Exit } from './exit';
 import { UsageFault } from './faults';
 import { ConsoleHost, Host, LoggingOptions } from './host';
 
+/**
+ * Command line arguments.
+ */
 export type Argv = typeof process.argv;
+
+/**
+ * Command line environment.
+ */
 export type Env = typeof process.env;
 
+/**
+ * Options for running a CLI.
+ */
 export interface CliOptions {
+  /**
+   * A Host instance. Defaults to ConsoleHost.
+   */
   host?: Host;
+
+  /**
+   * Command line arguments.
+   */
   argv: Argv;
+
+  /**
+   * Command line environment.
+   */
   env: Env;
 }
 
+/**
+ * A CLI command.
+ */
 export interface Cli<C extends LoggingOptions> {
+  /**
+   * Parse command line arguments and environment into a config.
+   *
+   * @param argv Command line arguments.
+   * @param env Command line environment.
+   * @returns A config.
+   */
   parseArgs(argv: typeof process.argv, env: typeof process.env): C;
+
+  /**
+   * Run the CLI command.
+   *
+   * @param config A config.
+   * @param host A Host.
+   * @returns A promise that resolves on completion.
+   */
   run(config: C, host: Host): Promise<void>;
 }
+
+/**
+ * A main function.
+ *
+ * @param options CLI options.
+ * @returns A promise that resolves on completion.
+ */
 export type Main = (options: CliOptions) => Promise<void>;
 
+/**
+ * Create a main function from a CLI command.
+ *
+ * @param cli A CLI command.
+ * @returns A main function.
+ */
 export function cli<C extends LoggingOptions>(cli: Cli<C>): Main {
   return async function main({
     host: defaultHost,
--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,12 @@ import { Argv, cli, Env } from './cli';
 import { Config } from './config';
 import { Host } from './host';
 
-export const main = cli({
-  parseArgs(argv: Argv, env: Env): Config {
-    return Config.load(argv, env);
-  },
-  async run(config: Config, host: Host): Promise<void> {
-    host.writeInfo(config.toString());
-  },
-});
+function parseArgs(argv: Argv, env: Env): Config {
+  return Config.load(argv, env);
+}
+
+async function run(config: Config, host: Host): Promise<void> {
+  host.writeInfo(config.toString());
+}
+
+export const main = cli({ parseArgs, run });

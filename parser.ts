dfeb677ad9727571a79f2ce4cbd4b46f51c9d9bd
Josh Holbrook <josh.holbrook@gmail.com>
2024-04-03 10:13:26 -0800

    Remove unused functions from parser

--- parser.ts
+++ parser.ts
@@ -165,30 +165,3 @@ export class Parser {
 // TODO: Parse Line
 // TODO: Parse Program
 // TODO: Sprinkle in Results everywhere
-
-/*
- * Parse a non-numbered command.
- *
- * @param source The source code.
- */
-export function parseCommand(source: string): null {
-  return null;
-}
-
-/*
- * Parse a numbered line.
- *
- * @param source The source code.
- */
-export function parseLine(source: string): null {
-  return null;
-}
-
-/*
- * Parse a program, made up of multipled numbered lines.
- *
- * @param source The source code.
- */
-export function parseProgram(source: string): null {
-  return null;
-}

c3cc8db50044859ee73ac06873f68ae4dc8c59c0
Josh Holbrook <josh.holbrook@gmail.com>
2024-04-03 14:15:42 -0800

    move Result to own module

--- parser.ts
+++ parser.ts
@@ -7,40 +7,13 @@ import {
   ParseWarning,
 } from './exceptions';
 import { Scanner } from './scanner';
+import { Result, Ok, Err, Warn } from './result';
 import { Token, TokenKind } from './tokens';
 
 import * as expr from './ast/expr';
 import { Cmd } from './ast/cmd';
 import { Line } from './ast/line';
 import { Program } from './ast/program';
-
-export abstract class Result<T, E, W = unknown> {
-  constructor(public result: T) {}
-}
-
-export class Ok<T, E, W = unknown> extends Result<T, E, W> {
-  constructor(result: T) {
-    super(result);
-  }
-}
-
-export class Err<T, E, W = unknown> extends Result<T, E, W> {
-  constructor(
-    result: T,
-    public error: E,
-  ) {
-    super(result);
-  }
-}
-
-export class Warn<T, E, W = unknown> extends Result<T, E, W> {
-  constructor(
-    result: T,
-    public warning: W,
-  ) {
-    super(result);
-  }
-}
 
 export type Output = Array<Line | Cmd[]>;
 
---
+++ result.ts
@@ -0,0 +1,27 @@
+export abstract class Result<T, E, W = unknown> {
+  constructor(public result: T) {}
+}
+
+export class Ok<T, E, W = unknown> extends Result<T, E, W> {
+  constructor(result: T) {
+    super(result);
+  }
+}
+
+export class Err<T, E, W = unknown> extends Result<T, E, W> {
+  constructor(
+    result: T,
+    public error: E,
+  ) {
+    super(result);
+  }
+}
+
+export class Warn<T, E, W = unknown> extends Result<T, E, W> {
+  constructor(
+    result: T,
+    public warning: W,
+  ) {
+    super(result);
+  }
+}

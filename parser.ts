6b29e5ba4e6fcafb28ce7e5f5f33890029e508ec
Josh Holbrook <josh.holbrook@gmail.com>
2024-04-03 09:22:14 -0800

    stub Parser class

--- parser.ts
+++ parser.ts
@@ -6,10 +6,101 @@ import {
   ParseWarning,
 } from './exceptions';
 import { Scanner } from './scanner';
-import { TokenKind } from './tokens';
+import { Token, TokenKind } from './tokens';
 
 import * as expr from './ast/expr';
-import * as cmd from './ast/cmd';
+import { Cmd } from './ast/cmd';
+import { Line } from './ast/line';
+import { Program } from './ast/program';
+
+/**
+ * Checks if a row ends in an EOF.
+ *
+ * @param row A row of tokens.
+ */
+export function isEnd(row: Token[]): boolean {
+  const kind = row[row.length - 1].kind;
+  return kind === TokenKind.Eof;
+}
+
+/**
+ * Checks if a row ends in a line ending.
+ *
+ * @param row A row of tokens.
+ */
+export function hasNextLine(row: Token[]): boolean {
+  return row[0].kind === TokenKind.LineEnding;
+}
+
+/**
+ * Checks if a row ends in either a line ending or an EOF.
+ *
+ * @param row A row of tokens.
+ */
+export function isTerminated(row: Token[]): boolean {
+  return isEnd(row) || hasNextLine(row);
+}
+
+export class ParseResult {
+  constructor(
+    public result: Array<Line | Cmd>,
+    public error: ParseError | ParseWarning | null = null,
+  ) {}
+}
+
+export class Parser {
+  private scanner: Scanner;
+
+  constructor(source: string) {
+    this.scanner = new Scanner(source);
+  }
+
+  /**
+   * Parse the source as input, returning a list of lines and commands.
+   *
+   * @returns A list of lines and commands.
+   */
+  public parseInput(): Array<Line | Cmd> {
+    let output: Array<Line | Cmd> = [];
+
+    let raiseWarning = false;
+    let raiseError = false;
+    const errors: Array<SyntaxError | SyntaxWarning> = [];
+
+    let row: Token[] = this.scanner.scanLine();
+
+    while (!isEnd(row)) {
+      const out = this.parseRow(row);
+      if (out instanceof Line) {
+        output.push(out);
+      } else {
+        output = output.concat(out);
+      }
+    }
+    return output;
+  }
+
+  /**
+   * Parse the source as a program, returning a Program.
+   *
+   * @returns A Program.
+   */
+  public parseProgram(): Program {
+    let output: Array<Line | Cmd> = this.parseInput();
+
+    for (let out of output) {
+      if (!(out instanceof Line)) {
+        throw new Error('lol');
+      }
+    }
+
+    return new Program(output as Line[]);
+  }
+
+  private parseRow(row: Token[]): Line | Cmd[] {
+    return [];
+  }
+}
 
 // TODO: Parse DecimalLiteral
 // TODO: Parse LineNo

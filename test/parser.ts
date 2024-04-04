e4cb4731a662ff2ab862c658035e537d524c1ce8
Josh Holbrook <josh.holbrook@gmail.com>
2024-04-03 16:27:24 -0800

    Stub parser tests

---
+++ test/parser.ts
@@ -0,0 +1,13 @@
+import t from 'tap';
+import { Test } from 'tap';
+
+// TODO: bare literal expressions
+// TODO: bare print statement
+// TODO: numbered literal expressions
+// TODO: numbered print statement
+// TODO: bare malformed print statement
+// TODO: numbered malformed print statement
+// TODO: multi-line numbered inputs
+// TODO: multi-line mixed inputs
+// TODO: multi-line numbered inputs with parseProgram
+// TODO: multi-line mixed inputs with parseProgram

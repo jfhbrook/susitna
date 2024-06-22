cd63878fbb6969640e41fd3131ef8f1f280196e8
Josh Holbrook <josh.holbrook@gmail.com>
2024-06-21 17:50:41 -0800

    was defining chunk twice in compiler

--- compiler.ts
+++ compiler.ts
@@ -46,7 +46,7 @@ export type CompilerOptions = {
 export class Compiler implements CmdVisitor<void>, ExprVisitor<void> {
   private ast: Cmd | Program | null;
 
-  private currentChunk: Chunk = new Chunk();
+  private currentChunk: Chunk;
   // TODO: Interactive commands are tracked by currentCmd, but programs are
   // tracked by currentCmdNo and currentLine. The two mechanisms feels a
   // little dirty. Would I rather create a "ghost line"?

b8cf51aa8ccac0f29262c8f4ed168ef9c5070aff
Josh Holbrook <josh.holbrook@gmail.com>
2024-06-24 22:00:16 -0800

    wc for drafts too

--- scripts/wordcount.sh
+++ scripts/wordcount.sh
@@ -2,8 +2,11 @@
 
 README_WC="$(wc -w < README.md)"
 ADR_WC="$(cat ./adrs/*.md | wc -w)"
+ADR_DRAFT_WC="$(cat ./adrs/draft/*.md | wc -w)"
 
-echo "project   count"
-echo "-------- ------"
-echo "README ${README_WC}"
-echo "adrs   ${ADR_WC}"
+echo "project     count"
+echo "--------   ------"
+echo "README   ${README_WC}"
+echo "adrs"
+echo " complete${ADR_WC}"
+echo " drafts  ${ADR_DRAFT_WC}"

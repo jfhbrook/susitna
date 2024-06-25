461a8f8b28127f2cbd9fb311fff4cbb87693b1e7
Josh Holbrook <josh.holbrook@gmail.com>
2024-06-24 21:49:38 -0800

    wc for README, not "main"

--- scripts/wordcount.sh
+++ scripts/wordcount.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-MATBAS_WC="$(wc -w < README.md)"
+README_WC="$(wc -w < README.md)"
 ADR_WC="$(cat ./adrs/*.md | wc -w)"
 
 echo "project   count"
 echo "-------- ------"
-echo "main   ${MATBAS_WC}"
+echo "README ${README_WC}"
 echo "adrs   ${ADR_WC}"

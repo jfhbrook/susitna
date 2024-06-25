ccf806cd3eb7a868fe9a9d27260d2cea81be441e
Josh Holbrook <josh.holbrook@gmail.com>
2024-06-24 20:42:43 -0800

    note on circular import bug

--- value/index.ts
+++ value/index.ts
@@ -1,3 +1,12 @@
+// TODO: Importing and re-exporting everything in here causes a really nasty
+// circular import bug. This can be seen in operations.ts - if it imports
+// from this file, it triggers the bug; but if it imports separately, the
+// issue doesn't occur.
+//
+// For now, I can avoid it by importing libraries directly, and it's probably
+// appropriate to deprecate this import path anyway. But I suspect that any
+// file which imports the wrong combination of these libraries could trigger
+// the issue.
 import { Nil, nil, Value } from './value';
 import { Type } from './types';
 import { typeOf } from './typeof';

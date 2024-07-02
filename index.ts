78093b3ab4ba708703437c764fbd3bc046bdca1c
Josh Holbrook <josh.holbrook@gmail.com>
2024-07-01 20:40:50 -0800

    Combine container and index

--- index.ts
+++ index.ts
@@ -6,23 +6,44 @@ if (MATBAS_BUILD === 'debug') {
   dotenv.config();
 }
 
+import { Module } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
-import { getTracer, NestLogger } from './debug';
-import { Container } from './container';
-import { Main } from './cli';
+import { NestLogger } from './debug';
 
-const tracer = getTracer('main');
+import { Config, Argv, Env } from './config';
+import { Main } from './cli';
+import { ConsoleHost } from './host';
+import { Editor } from './editor';
+import { Executor } from './executor';
+
+@Module({
+  providers: [
+    { provide: 'argv', useValue: process.argv.slice(2) },
+    { provide: 'env', useValue: process.env },
+    { provide: 'exitFn', useValue: process.exit },
+    {
+      provide: Config,
+      useFactory: (argv: Argv, env: Env) => {
+        return Config.load(argv, env);
+      },
+      inject: ['argv', 'env'],
+    },
+    {
+      provide: 'Host',
+      useClass: ConsoleHost,
+    },
+    Editor,
+    Executor,
+    Main,
+  ],
+})
+export class Container {}
 
 export async function main(): Promise<void> {
-  tracer.open('main');
   const deps = await NestFactory.createApplicationContext(Container, {
     logger: new NestLogger(),
   });
   const main = deps.get(Main);
-  try {
-    await main.start();
-  } finally {
-    tracer.close();
-  }
+  await main.start();
 }

69645d2ff6bc2b5b6c0bfeb511f4cd74c0877580
Josh Holbrook <josh.holbrook@gmail.com>
2024-09-15 08:19:13 -0800

    Fix citree tests

--- tools/citree/test/int.ts
+++ tools/citree/test/int.ts
@@ -8,7 +8,7 @@ import main from '../src/index';
 t.test('integration', async (t) => {
   await main([resolve(join(__dirname, '../example/ast.citree'))]);
 
-  for (let f of ['cmd.ts', 'expr.ts', 'index.ts']) {
+  for (let f of ['instr.ts', 'expr.ts', 'index.ts']) {
     const out = readFileSync(
       resolve(join(__dirname, '../example', f)),
       'utf8',

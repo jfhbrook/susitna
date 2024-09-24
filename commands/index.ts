jfhbrook/matanuska

AST support for ShortIf and If

commands/index.ts
@@ -37,6 +37,8 @@ export function commandRunner(
     visitExitCmd: invalid('exit'),
     visitLetCmd: invalid('let'),
     visitAssignCmd: invalid('assign'),
+    visitShortIfCmd: invalid('if'),
+    visitIfCmd: invalid('if'),
   };
 }
 

compiler.ts
@@ -5,6 +5,7 @@ import {
   ParseError,
   ParseWarning,
   mergeParseErrors,
+  NotImplementedError,
 } from './exceptions';
 import { runtimeMethod } from './faults';
 import { Token, TokenKind } from './tokens';
@@ -28,6 +29,8 @@ import {
   Exit,
   Let,
   Assign,
+  ShortIf,
+  If,
 } from './ast/cmd';
 import {
   Expr,
@@ -406,6 +409,14 @@ export class LineCompiler implements CmdVisitor<void>, ExprVisitor<void> {
     });
   }
 
+  visitShortIfCmd(_if: ShortIf): void {
+    throw new NotImplementedError('ShortIf');
+  }
+
+  visitIfCmd(_if: If): void {
+    throw new NotImplementedError('If');
+  }
+
   // Expressions
 
   visitUnaryExpr(unary: Unary): void {
@@ -628,6 +639,14 @@ export class CommandCompiler implements CmdVisitor<CompileResult<CompiledCmd>> {
 
   visitAssignCmd(assign: Assign): CompileResult<CompiledCmd> {
     return this.compiled(assign);
+  }
+
+  visitShortIfCmd(if_: ShortIf): CompileResult<CompiledCmd> {
+    return this.compiled(if_);
+  }
+
+  visitIfCmd(if_: If): CompileResult<CompiledCmd> {
+    return this.compiled(if_);
   }
 }
 

format.ts
@@ -52,6 +52,8 @@ import {
   Exit as ExitCmd,
   Let,
   Assign,
+  ShortIf,
+  If,
 } from './ast/cmd';
 import { Tree, TreeVisitor, CommandGroup, Line, Input, Program } from './ast';
 import { Token } from './tokens';
@@ -180,6 +182,8 @@ export abstract class Formatter
   abstract visitAssignCmd(assign: Assign): string;
   abstract visitEndCmd(end: End): string;
   abstract visitExitCmd(exit: ExitCmd): string;
+  abstract visitShortIfCmd(if_: ShortIf): string;
+  abstract visitIfCmd(if_: If): string;
 
   abstract visitCommandGroupTree(node: CommandGroup): string;
   abstract visitLineTree(node: Line): string;
@@ -643,6 +647,39 @@ export class DefaultFormatter extends Formatter {
 
   visitAssignCmd(assign: Assign): string {
     return `Assign(${this.format(assign.variable)}, ${this.format(assign.value)})`;
+  }
+
+  visitShortIfCmd(if_: ShortIf): string {
+    let formatted = `ShortIf (${this.format(if_.condition)}) { `;
+    formatted += if_.then.map((c) => this.format(c)).join(' : ') + ' }';
+    if (if_.else_.length) {
+      formatted +=
+        ' { ' + if_.else_.map((c) => this.format(c)).join(' : ') + ' }';
+    }
+    return formatted;
+  }
+
+  visitIfCmd(if_: If): string {
+    let formatted = `If (${this.format(if_.condition)}) {\n`;
+
+    for (const line of if_.then) {
+      formatted += indent(1, `${this.format(line)},\n`);
+    }
+
+    if (if_.else_ instanceof If) {
+      formatted += '} Else ';
+      formatted += this.format(if_.else_);
+    } else if (if_.else_.length) {
+      formatted += '} Else {\n';
+      for (const line of if_.else_) {
+        formatted += indent(1, `${this.format(line)},\n`);
+      }
+      formatted += '}';
+    } else {
+      formatted += '}';
+    }
+
+    return formatted;
   }
 
   formatStack<V>(stack: Stack<V>): string {

tap-snapshots/test/format.ts.test.cjs
@@ -6,34 +6,78 @@
  */
 'use strict'
 exports[`test/format.ts > TAP > given a DefaultFormatter > commands > must match snapshot 1`] = `
-Exit(0)
+Print('hello')
+`
+
+exports[`test/format.ts > TAP > given a DefaultFormatter > commands > must match snapshot 10`] = `
+ShortIf (true) { Print('true') }
+`
+
+exports[`test/format.ts > TAP > given a DefaultFormatter > commands > must match snapshot 11`] = `
+ShortIf (true) { Print('true') } { Print('false') }
+`
+
+exports[`test/format.ts > TAP > given a DefaultFormatter > commands > must match snapshot 12`] = `
+If (true) {
+  Line(20) [
+    Print('hello world'),
+  ],
+}
+`
+
+exports[`test/format.ts > TAP > given a DefaultFormatter > commands > must match snapshot 13`] = `
+If (true) {
+  Line(20) [
+    Print('hello world'),
+  ],
+} Else {
+  Line(20) [
+    Print('hello world'),
+  ],
+}
+`
+
+exports[`test/format.ts > TAP > given a DefaultFormatter > commands > must match snapshot 14`] = `
+If (true) {
+  Line(20) [
+    Print('true'),
+  ],
+} Else If (true) {
+  Line(20) [
+    Print('false, but true'),
+  ],
+}
 `
 
 exports[`test/format.ts > TAP > given a DefaultFormatter > commands > must match snapshot 2`] = `
-Rem(a witty remark)
+Exit(0)
 `
 
 exports[`test/format.ts > TAP > given a DefaultFormatter > commands > must match snapshot 3`] = `
-New(null)
+Rem(a witty remark)
 `
 
 exports[`test/format.ts > TAP > given a DefaultFormatter > commands > must match snapshot 4`] = `
-Load('./examples/001-hello-world.bas', run=true)
-`
-
-exports[`test/format.ts > TAP > given a DefaultFormatter > commands > must match snapshot 5`] = `
-List
-`
-
-exports[`test/format.ts > TAP > given a DefaultFormatter > commands > must match snapshot 6`] = `
 New(null)
 `
 
+exports[`test/format.ts > TAP > given a DefaultFormatter > commands > must match snapshot 5`] = `
+Load('./examples/001-hello-world.bas', run=true)
+`
+
+exports[`test/format.ts > TAP > given a DefaultFormatter > commands > must match snapshot 6`] = `
+List
+`
+
 exports[`test/format.ts > TAP > given a DefaultFormatter > commands > must match snapshot 7`] = `
-Run
+New(null)
 `
 
 exports[`test/format.ts > TAP > given a DefaultFormatter > commands > must match snapshot 8`] = `
+Run
+`
+
+exports[`test/format.ts > TAP > given a DefaultFormatter > commands > must match snapshot 9`] = `
 Let(i%, 1)
 `
 
@@ -48,7 +92,7 @@ exports[`test/format.ts > TAP > given a DefaultFormatter > it formats a BaseFaul
 
 --- Internal Stack Trace ---
 BaseFault: message
-    at Test.<anonymous> ({CWD}/test/format.ts:266:40)
+    at Test.<anonymous> ({CWD}/test/format.ts:268:40)
     at TapWrap.runInAsyncScope (node:async_hooks:206:9)
     at Test.cb ({CWD}/node_modules/@tapjs/core/src/test-base.ts:338:17)
     at <anonymous> ({CWD}/node_modules/@tapjs/core/src/test-base.ts:1153:21)
@@ -81,10 +125,6 @@ true
 
 exports[`test/format.ts > TAP > given a DefaultFormatter > it formats a BoolLiteral > must match snapshot 1`] = `
 true
-`
-
-exports[`test/format.ts > TAP > given a DefaultFormatter > it formats a Cmd > must match snapshot 1`] = `
-Print('hello')
 `
 
 exports[`test/format.ts > TAP > given a DefaultFormatter > it formats a Exception > must match snapshot 1`] = `
@@ -164,7 +204,7 @@ exports[`test/format.ts > TAP > given a DefaultFormatter > it formats a RuntimeF
 --- Internal Stack Trace ---
 AssertionError [ERR_ASSERTION]: underlying assertion
     at new AssertionError (node:internal/assert/assertion_error:451:5)
-    at Test.<anonymous> ({CWD}/test/format.ts:270:26)
+    at Test.<anonymous> ({CWD}/test/format.ts:272:26)
     at TapWrap.runInAsyncScope (node:async_hooks:206:9)
     at Test.cb ({CWD}/node_modules/@tapjs/core/src/test-base.ts:338:17)
     at <anonymous> ({CWD}/node_modules/@tapjs/core/src/test-base.ts:1153:21)
@@ -334,34 +374,78 @@ exports[`test/format.ts > TAP > given a DefaultFormatter > when it is not a line
 `
 
 exports[`test/format.ts > TAP > given a Inspector > commands > must match snapshot 1`] = `
-Exit(0)
+Print('hello')
+`
+
+exports[`test/format.ts > TAP > given a Inspector > commands > must match snapshot 10`] = `
+ShortIf (true) { Print('true') }
+`
+
+exports[`test/format.ts > TAP > given a Inspector > commands > must match snapshot 11`] = `
+ShortIf (true) { Print('true') } { Print('false') }
+`
+
+exports[`test/format.ts > TAP > given a Inspector > commands > must match snapshot 12`] = `
+If (true) {
+  Line(20) [
+    Print('hello world'),
+  ],
+}
+`
+
+exports[`test/format.ts > TAP > given a Inspector > commands > must match snapshot 13`] = `
+If (true) {
+  Line(20) [
+    Print('hello world'),
+  ],
+} Else {
+  Line(20) [
+    Print('hello world'),
+  ],
+}
+`
+
+exports[`test/format.ts > TAP > given a Inspector > commands > must match snapshot 14`] = `
+If (true) {
+  Line(20) [
+    Print('true'),
+  ],
+} Else If (true) {
+  Line(20) [
+    Print('false, but true'),
+  ],
+}
 `
 
 exports[`test/format.ts > TAP > given a Inspector > commands > must match snapshot 2`] = `
-Rem(a witty remark)
+Exit(0)
 `
 
 exports[`test/format.ts > TAP > given a Inspector > commands > must match snapshot 3`] = `
-New([1mnull[22m)
+Rem(a witty remark)
 `
 
 exports[`test/format.ts > TAP > given a Inspector > commands > must match snapshot 4`] = `
-Load('./examples/001-hello-world.bas', run=true)
-`
-
-exports[`test/format.ts > TAP > given a Inspector > commands > must match snapshot 5`] = `
-List
-`
-
-exports[`test/format.ts > TAP > given a Inspector > commands > must match snapshot 6`] = `
 New([1mnull[22m)
 `
 
+exports[`test/format.ts > TAP > given a Inspector > commands > must match snapshot 5`] = `
+Load('./examples/001-hello-world.bas', run=true)
+`
+
+exports[`test/format.ts > TAP > given a Inspector > commands > must match snapshot 6`] = `
+List
+`
+
 exports[`test/format.ts > TAP > given a Inspector > commands > must match snapshot 7`] = `
-Run
+New([1mnull[22m)
 `
 
 exports[`test/format.ts > TAP > given a Inspector > commands > must match snapshot 8`] = `
+Run
+`
+
+exports[`test/format.ts > TAP > given a Inspector > commands > must match snapshot 9`] = `
 Let(i%, 1)
 `
 
@@ -376,7 +460,7 @@ exports[`test/format.ts > TAP > given a Inspector > it formats a BaseFault > mus
 
 --- Internal Stack Trace ---
 BaseFault: message
-    at Test.<anonymous> ({CWD}/test/format.ts:266:40)
+    at Test.<anonymous> ({CWD}/test/format.ts:268:40)
     at TapWrap.runInAsyncScope (node:async_hooks:206:9)
     at Test.cb ({CWD}/node_modules/@tapjs/core/src/test-base.ts:338:17)
     at <anonymous> ({CWD}/node_modules/@tapjs/core/src/test-base.ts:1153:21)
@@ -409,10 +493,6 @@ exports[`test/format.ts > TAP > given a Inspector > it formats a boolean > must 
 
 exports[`test/format.ts > TAP > given a Inspector > it formats a BoolLiteral > must match snapshot 1`] = `
 true
-`
-
-exports[`test/format.ts > TAP > given a Inspector > it formats a Cmd > must match snapshot 1`] = `
-Print('hello')
 `
 
 exports[`test/format.ts > TAP > given a Inspector > it formats a Exception > must match snapshot 1`] = `
@@ -492,7 +572,7 @@ exports[`test/format.ts > TAP > given a Inspector > it formats a RuntimeFault > 
 --- Internal Stack Trace ---
 AssertionError [ERR_ASSERTION]: underlying assertion
     at new AssertionError (node:internal/assert/assertion_error:451:5)
-    at Test.<anonymous> ({CWD}/test/format.ts:270:26)
+    at Test.<anonymous> ({CWD}/test/format.ts:272:26)
     at TapWrap.runInAsyncScope (node:async_hooks:206:9)
     at Test.cb ({CWD}/node_modules/@tapjs/core/src/test-base.ts:338:17)
     at <anonymous> ({CWD}/node_modules/@tapjs/core/src/test-base.ts:1153:21)

jfhbrook/susitna

commands/index.ts
@@ -37,6 +37,8 @@ export function commandRunner(
     visitExitInstr: invalid('exit'),
     visitLetInstr: invalid('let'),
     visitAssignInstr: invalid('assign'),
+    visitShortIfInstr: invalid('if'),
+    visitIfInstr: invalid('if'),
   };
 }
 

compiler.ts
@@ -5,6 +5,7 @@ import {
   ParseError,
   ParseWarning,
   mergeParseErrors,
+  NotImplementedError,
 } from './exceptions';
 import { runtimeMethod } from './faults';
 import { Token, TokenKind } from './tokens';
@@ -28,6 +29,8 @@ import {
   Exit,
   Let,
   Assign,
+  ShortIf,
+  If,
 } from './ast/instr';
 import {
   Expr,
@@ -408,6 +411,14 @@ export class LineCompiler implements InstrVisitor<void>, ExprVisitor<void> {
     });
   }
 
+  visitShortIfInstr(_if: ShortIf): void {
+    throw new NotImplementedError('ShortIf');
+  }
+
+  visitIfInstr(_if: If): void {
+    throw new NotImplementedError('If');
+  }
+
   // Expressions
 
   visitUnaryExpr(unary: Unary): void {
@@ -632,6 +643,14 @@ export class CommandCompiler
 
   visitAssignInstr(assign: Assign): CompileResult<CompiledCmd> {
     return this.runtime(assign);
+  }
+
+  visitShortIfInstr(if_: ShortIf): CompileResult<CompiledCmd> {
+    return this.runtime(if_);
+  }
+
+  visitIfInstr(if_: If): CompileResult<CompiledCmd> {
+    return this.runtime(if_);
   }
 }
 

format.ts
@@ -52,6 +52,8 @@ import {
   Exit as ExitInstr,
   Let,
   Assign,
+  ShortIf,
+  If,
 } from './ast/instr';
 import { Tree, TreeVisitor, Cmd, Line, Input, Program } from './ast';
 import { Token } from './tokens';
@@ -180,6 +182,8 @@ export abstract class Formatter
   abstract visitAssignInstr(assign: Assign): string;
   abstract visitEndInstr(end: End): string;
   abstract visitExitInstr(exit: ExitInstr): string;
+  abstract visitShortIfInstr(if_: ShortIf): string;
+  abstract visitIfInstr(if_: If): string;
 
   abstract visitCmdTree(node: Cmd): string;
   abstract visitLineTree(node: Line): string;
@@ -643,6 +647,39 @@ export class DefaultFormatter extends Formatter {
 
   visitAssignInstr(assign: Assign): string {
     return `Assign(${this.format(assign.variable)}, ${this.format(assign.value)})`;
+  }
+
+  visitShortIfInstr(if_: ShortIf): string {
+    let formatted = `ShortIf (${this.format(if_.condition)}) { `;
+    formatted += if_.then.map((c) => this.format(c)).join(' : ') + ' }';
+    if (if_.else_.length) {
+      formatted +=
+        ' { ' + if_.else_.map((c) => this.format(c)).join(' : ') + ' }';
+    }
+    return formatted;
+  }
+
+  visitIfInstr(if_: If): string {
+    let formatted = `If (${this.format(if_.condition)}) {\n`;
+
+    for (const line of if_.then) {
+      formatted += indent(1, `${this.format(line)},\n`);
+    }
+
+    if (if_.else_ instanceof If) {
+      formatted += '} Else ';
+      formatted += this.format(if_.else_);
+    } else if (if_.else_.length) {
+      formatted += '} Else {\n';
+      for (const line of if_.else_) {
+        formatted += indent(1, `${this.format(line)},\n`);
+      }
+      formatted += '}';
+    } else {
+      formatted += '}';
+    }
+
+    return formatted;
   }
 
   formatStack<V>(stack: Stack<V>): string {

tap-snapshots/test/format.ts.test.cjs
@@ -6,34 +6,78 @@
  */
 'use strict'
 exports[`test/format.ts > TAP > given a DefaultFormatter > instructions > must match snapshot 1`] = `
-Exit(0)
+Print('hello')
+`
+
+exports[`test/format.ts > TAP > given a DefaultFormatter > instructions > must match snapshot 10`] = `
+ShortIf (true) { Print('true') }
+`
+
+exports[`test/format.ts > TAP > given a DefaultFormatter > instructions > must match snapshot 11`] = `
+ShortIf (true) { Print('true') } { Print('false') }
+`
+
+exports[`test/format.ts > TAP > given a DefaultFormatter > instructions > must match snapshot 12`] = `
+If (true) {
+  Line(20) [
+    Print('hello world'),
+  ],
+}
+`
+
+exports[`test/format.ts > TAP > given a DefaultFormatter > instructions > must match snapshot 13`] = `
+If (true) {
+  Line(20) [
+    Print('hello world'),
+  ],
+} Else {
+  Line(20) [
+    Print('hello world'),
+  ],
+}
+`
+
+exports[`test/format.ts > TAP > given a DefaultFormatter > instructions > must match snapshot 14`] = `
+If (true) {
+  Line(20) [
+    Print('true'),
+  ],
+} Else If (true) {
+  Line(20) [
+    Print('false, but true'),
+  ],
+}
 `
 
 exports[`test/format.ts > TAP > given a DefaultFormatter > instructions > must match snapshot 2`] = `
-Rem(a witty remark)
+Exit(0)
 `
 
 exports[`test/format.ts > TAP > given a DefaultFormatter > instructions > must match snapshot 3`] = `
-New(null)
+Rem(a witty remark)
 `
 
 exports[`test/format.ts > TAP > given a DefaultFormatter > instructions > must match snapshot 4`] = `
-Load('./examples/001-hello-world.bas', run=true)
-`
-
-exports[`test/format.ts > TAP > given a DefaultFormatter > instructions > must match snapshot 5`] = `
-List
-`
-
-exports[`test/format.ts > TAP > given a DefaultFormatter > instructions > must match snapshot 6`] = `
 New(null)
 `
 
+exports[`test/format.ts > TAP > given a DefaultFormatter > instructions > must match snapshot 5`] = `
+Load('./examples/001-hello-world.bas', run=true)
+`
+
+exports[`test/format.ts > TAP > given a DefaultFormatter > instructions > must match snapshot 6`] = `
+List
+`
+
 exports[`test/format.ts > TAP > given a DefaultFormatter > instructions > must match snapshot 7`] = `
-Run
+New(null)
 `
 
 exports[`test/format.ts > TAP > given a DefaultFormatter > instructions > must match snapshot 8`] = `
+Run
+`
+
+exports[`test/format.ts > TAP > given a DefaultFormatter > instructions > must match snapshot 9`] = `
 Let(i%, 1)
 `
 
@@ -48,7 +92,7 @@ exports[`test/format.ts > TAP > given a DefaultFormatter > it formats a BaseFaul
 
 --- Internal Stack Trace ---
 BaseFault: message
-    at Test.<anonymous> ({CWD}/test/format.ts:266:40)
+    at Test.<anonymous> ({CWD}/test/format.ts:268:40)
     at TapWrap.runInAsyncScope (node:async_hooks:211:14)
     at Test.cb ({CWD}/node_modules/@tapjs/core/src/test-base.ts:338:17)
     at <anonymous> ({CWD}/node_modules/@tapjs/core/src/test-base.ts:1153:21)
@@ -164,7 +208,7 @@ exports[`test/format.ts > TAP > given a DefaultFormatter > it formats a RuntimeF
 --- Internal Stack Trace ---
 AssertionError [ERR_ASSERTION]: underlying assertion
     at new AssertionError (node:internal/assert/assertion_error:447:5)
-    at Test.<anonymous> ({CWD}/test/format.ts:270:26)
+    at Test.<anonymous> ({CWD}/test/format.ts:272:26)
     at TapWrap.runInAsyncScope (node:async_hooks:211:14)
     at Test.cb ({CWD}/node_modules/@tapjs/core/src/test-base.ts:338:17)
     at <anonymous> ({CWD}/node_modules/@tapjs/core/src/test-base.ts:1153:21)
@@ -334,34 +378,78 @@ exports[`test/format.ts > TAP > given a DefaultFormatter > when it is not a line
 `
 
 exports[`test/format.ts > TAP > given a Inspector > instructions > must match snapshot 1`] = `
-Exit(0)
+Print('hello')
+`
+
+exports[`test/format.ts > TAP > given a Inspector > instructions > must match snapshot 10`] = `
+ShortIf (true) { Print('true') }
+`
+
+exports[`test/format.ts > TAP > given a Inspector > instructions > must match snapshot 11`] = `
+ShortIf (true) { Print('true') } { Print('false') }
+`
+
+exports[`test/format.ts > TAP > given a Inspector > instructions > must match snapshot 12`] = `
+If (true) {
+  Line(20) [
+    Print('hello world'),
+  ],
+}
+`
+
+exports[`test/format.ts > TAP > given a Inspector > instructions > must match snapshot 13`] = `
+If (true) {
+  Line(20) [
+    Print('hello world'),
+  ],
+} Else {
+  Line(20) [
+    Print('hello world'),
+  ],
+}
+`
+
+exports[`test/format.ts > TAP > given a Inspector > instructions > must match snapshot 14`] = `
+If (true) {
+  Line(20) [
+    Print('true'),
+  ],
+} Else If (true) {
+  Line(20) [
+    Print('false, but true'),
+  ],
+}
 `
 
 exports[`test/format.ts > TAP > given a Inspector > instructions > must match snapshot 2`] = `
-Rem(a witty remark)
+Exit(0)
 `
 
 exports[`test/format.ts > TAP > given a Inspector > instructions > must match snapshot 3`] = `
-New([1mnull[22m)
+Rem(a witty remark)
 `
 
 exports[`test/format.ts > TAP > given a Inspector > instructions > must match snapshot 4`] = `
-Load('./examples/001-hello-world.bas', run=true)
-`
-
-exports[`test/format.ts > TAP > given a Inspector > instructions > must match snapshot 5`] = `
-List
-`
-
-exports[`test/format.ts > TAP > given a Inspector > instructions > must match snapshot 6`] = `
 New([1mnull[22m)
 `
 
+exports[`test/format.ts > TAP > given a Inspector > instructions > must match snapshot 5`] = `
+Load('./examples/001-hello-world.bas', run=true)
+`
+
+exports[`test/format.ts > TAP > given a Inspector > instructions > must match snapshot 6`] = `
+List
+`
+
 exports[`test/format.ts > TAP > given a Inspector > instructions > must match snapshot 7`] = `
-Run
+New([1mnull[22m)
 `
 
 exports[`test/format.ts > TAP > given a Inspector > instructions > must match snapshot 8`] = `
+Run
+`
+
+exports[`test/format.ts > TAP > given a Inspector > instructions > must match snapshot 9`] = `
 Let(i%, 1)
 `
 
@@ -376,7 +464,7 @@ exports[`test/format.ts > TAP > given a Inspector > it formats a BaseFault > mus
 
 --- Internal Stack Trace ---
 BaseFault: message
-    at Test.<anonymous> ({CWD}/test/format.ts:266:40)
+    at Test.<anonymous> ({CWD}/test/format.ts:268:40)
     at TapWrap.runInAsyncScope (node:async_hooks:211:14)
     at Test.cb ({CWD}/node_modules/@tapjs/core/src/test-base.ts:338:17)
     at <anonymous> ({CWD}/node_modules/@tapjs/core/src/test-base.ts:1153:21)
@@ -492,7 +580,7 @@ exports[`test/format.ts > TAP > given a Inspector > it formats a RuntimeFault > 
 --- Internal Stack Trace ---
 AssertionError [ERR_ASSERTION]: underlying assertion
     at new AssertionError (node:internal/assert/assertion_error:447:5)
-    at Test.<anonymous> ({CWD}/test/format.ts:270:26)
+    at Test.<anonymous> ({CWD}/test/format.ts:272:26)
     at TapWrap.runInAsyncScope (node:async_hooks:211:14)
     at Test.cb ({CWD}/node_modules/@tapjs/core/src/test-base.ts:338:17)
     at <anonymous> ({CWD}/node_modules/@tapjs/core/src/test-base.ts:1153:21)
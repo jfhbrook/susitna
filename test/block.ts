0ec231a9c4dc381f1080211fc770b779de9ccb79
Josh Holbrook <josh.holbrook@gmail.com>
2024-09-29 16:22:33 -0800

    test invalid/mismatched instructions in base Block

--- test/block.ts
+++ test/block.ts
@@ -1,8 +1,27 @@
 import t from 'tap';
 import { Test } from 'tap';
 
+import { formatter } from '../format';
+
 import { StringLiteral } from '../ast/expr';
-import { Print } from '../ast/instr';
+import {
+  Print,
+  Expression,
+  Rem,
+  New,
+  Load,
+  List,
+  Renum,
+  Save,
+  Run,
+  Exit,
+  End,
+  If,
+  ShortIf,
+  Else,
+  ElseIf,
+  EndIf,
+} from '../ast/instr';
 import { Block } from '../compiler/block';
 import { LineCompiler, RoutineType } from '../compiler/base';
 
@@ -14,7 +33,7 @@ class TestBlock extends Block {
   }
 }
 
-const INSTR = new Print(new StringLiteral('hello world'));
+const PRINT = new Print(new StringLiteral('hello world'));
 
 function createCompiler(): LineCompiler {
   const compiler = new LineCompiler([], RoutineType.Program, {
@@ -29,6 +48,28 @@ function createBlock(compiler: LineCompiler, name: string): TestBlock {
   return block;
 }
 
+const INVALID_INSTRS = [
+  PRINT,
+  new Expression(new StringLiteral('a')),
+  new Rem('remark'),
+  new New(null),
+  new Load(new StringLiteral('file'), false),
+  new List(),
+  new Renum(),
+  new Save(new StringLiteral('file')),
+  new Run(),
+  new Exit(null),
+  new End(),
+  new If(new StringLiteral('foo')),
+  new ShortIf(new StringLiteral('foo'), [], []),
+];
+
+const MISMATCHED_INSTRS = [
+  new Else(),
+  new ElseIf(new StringLiteral('bar')),
+  new EndIf(),
+];
+
 t.test('Block', async (t: Test) => {
   await t.test('begin/end', async (t: Test) => {
     const compiler = createCompiler();
@@ -37,10 +78,10 @@ t.test('Block', async (t: Test) => {
 
     t.equal(compiler.block, root, 'Current block is root');
 
-    compiler.block.begin(INSTR, block);
+    compiler.block.begin(PRINT, block);
 
     t.equal(compiler.block, block, 'Current block is block');
-    t.equal(block.instr, INSTR, 'instr is set by begin');
+    t.equal(block.instr, PRINT, 'instr is set by begin');
     t.equal(block.previous, null, 'b has no previous block');
     t.equal(block.parent, root, "b's parent block is root");
 
@@ -53,16 +94,45 @@ t.test('Block', async (t: Test) => {
     const compiler = createCompiler();
     const root = compiler.block;
     const a = createBlock(compiler, 'a');
-    compiler.block.begin(INSTR, a);
+    compiler.block.begin(PRINT, a);
     const b = createBlock(compiler, 'b');
 
     t.equal(compiler.block, a, 'Current block is a');
 
-    a.next(INSTR, b);
+    a.next(PRINT, b);
 
     t.equal(compiler.block, b, 'Current block is b');
-    t.equal(b.instr, INSTR, 'instr is set by begin');
+    t.equal(b.instr, PRINT, 'instr is set by begin');
     t.equal(b.previous, a, 'a is the previous block');
     t.equal(b.parent, root, "b's parent block is root");
+  });
+
+  // TODO: Assert the error messages...
+  await t.test('handle', async (t: Test) => {
+    for (const instr of INVALID_INSTRS) {
+      await t.test(
+        `invalid instruction ${formatter.format(instr)}`,
+        async (t: Test) => {
+          const compiler = createCompiler();
+          const block = createBlock(compiler, 'block');
+          t.throws(() => {
+            block.handle(instr);
+          });
+        },
+      );
+    }
+
+    for (const instr of MISMATCHED_INSTRS) {
+      await t.test(
+        `mismatched instruction ${formatter.format(instr)}`,
+        async (t: Test) => {
+          const compiler = createCompiler();
+          const block = createBlock(compiler, 'block');
+          t.throws(() => {
+            block.handle(instr);
+          });
+        },
+      );
+    }
   });
 });

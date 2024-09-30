5325ff1a5f39fedc721aafdc9a011340200ea1d3
Josh Holbrook <josh.holbrook@gmail.com>
2024-09-30 06:40:15 -0800

    Failing tests for renum

--- tap-snapshots/test/editor.ts.test.cjs
+++ tap-snapshots/test/editor.ts.test.cjs
@@ -5,8 +5,20 @@
  * Make sure to inspect the output below.  Do not ignore changes!
  */
 'use strict'
-exports[`test/editor.ts > TAP > editor > must match snapshot 1`] = `
+exports[`test/editor.ts > TAP > editor inserts > must match snapshot 1`] = `
 10 print "hello"
 15
 30 print "world"
+`
+
+exports[`test/editor.ts > TAP > editor renum > left justified > must match snapshot 1`] = `
+
+`
+
+exports[`test/editor.ts > TAP > editor renum > mixed double/triple to all double > must match snapshot 1`] = `
+
+`
+
+exports[`test/editor.ts > TAP > editor renum > right justified > must match snapshot 1`] = `
+
 `

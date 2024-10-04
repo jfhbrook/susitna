d32a9eb4e87096e3c1c9012281f9177e5845b278
Josh Holbrook <josh.holbrook@gmail.com>
2024-10-03 18:31:41 -0800

    Tweak build config options

--- scripts/build-config.mjs
+++ scripts/build-config.mjs
@@ -6,34 +6,49 @@ const exclude = ['node_modules', 'bin', 'tools', 'dist'];
 const tscExclude = ['*.njk.ts'];
 const swcExclude = ['.njk.ts'];
 const testExclude = ['test'];
+
 const baseUrl = './';
 const outDir = './dist';
 const target = 'es2022';
-const enableDecorators = true;
 const moduleType = 'commonjs';
+
+const sourceMaps = true;
+// Necessary for NestJS to work
+const decorators = true;
+
+// Type checks - note, only tsc does type checking
+const strictNullChecks = true;
+const noImplicitAny = false;
+const strictBindCallApply = false;
+const forceConsistentCasingInFileNames = true;
+const noFallthroughCasesInSwitch = false;
+
+const minify = true; // swc only
+const declaration = true; // tsc only
+const removeComments = true; // tsc only
 
 const tscConfig = {
   exclude: exclude.concat(tscExclude),
   compilerOptions: {
     module: moduleType,
-    declaration: true,
     baseUrl,
     outDir,
     target: target,
     lib: [target],
-    sourceMap: true,
-    removeComments: true,
-    emitDecoratorMetadata: enableDecorators,
-    experimentalDecorators: enableDecorators,
-    allowSyntheticDefaultImports: true,
-    incremental: true,
-    skipLibCheck: true,
-    strictNullChecks: true,
-    noImplicitAny: false,
-    strictBindCallApply: false,
-    forceConsistentCasingInFileNames: true,
-    noFallthroughCasesInSwitch: false,
     esModuleInterop: true,
+    incremental: true,
+    sourceMap: sourceMaps,
+    emitDecoratorMetadata: decorators,
+    experimentalDecorators: decorators,
+    declaration,
+    removeComments,
+    allowSyntheticDefaultImports: true,
+    skipLibCheck: true,
+    strictNullChecks,
+    noImplicitAny,
+    strictBindCallApply,
+    forceConsistentCasingInFileNames,
+    noFallthroughCasesInSwitch,
   },
 };
 
@@ -45,19 +60,19 @@ const swcConfig = {
     parser: {
       syntax: 'typescript',
       jsx: false,
+      decorators: decorators,
+      decoratorsBeforeExport: decorators,
       dynamicImport: true,
       privateMethod: true,
       functionBind: true,
-      exportDefaultFrom: false,
-      exportNamespaceFrom: false,
-      decorators: enableDecorators,
-      decoratorsBeforeExport: enableDecorators,
+      exportDefaultFrom: true,
+      exportNamespaceFrom: true,
       topLevelAwait: false,
       importMeta: false,
     },
     transform: {
       legacyDecorator: false,
-      decoratorMetadata: enableDecorators,
+      decoratorMetadata: decorators,
     },
     target: target,
     loose: false,
@@ -67,7 +82,9 @@ const swcConfig = {
   module: {
     type: moduleType,
   },
-  minify: false,
+  sourceMaps,
+  inlineSourcesContent: sourceMaps,
+  minify,
 };
 
 function writeConfig(filename, config) {
@@ -76,8 +93,11 @@ function writeConfig(filename, config) {
 
 writeConfig('tsconfig.json', tscConfig);
 writeConfig('tsconfig.build.json', {
-  extends: "./tsconfig.json",
-  exclude: exclude.concat(testExclude)
+  extends: './tsconfig.json',
+  exclude: exclude.concat(testExclude),
 });
 writeConfig('./.swcrc', swcConfig);
-writeConfig('./.swcrc.build', {...swcConfig, exclude: exclude.concat(testExclude) });
+writeConfig('./.swcrc.build', {
+  ...swcConfig,
+  exclude: exclude.concat(testExclude),
+});

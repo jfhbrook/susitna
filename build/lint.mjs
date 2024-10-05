421d9e46a932991353f2583d1152c151c6e3f0f8
Josh Holbrook <josh.holbrook@gmail.com>
2024-10-05 11:59:03 -0800

    Simplify lint ignores

--- build/lint.mjs
+++ build/lint.mjs
@@ -2,5 +2,7 @@ import { merge } from './util.mjs';
 
 import config from './config.json' with { type: 'json' };
 
-export const ignoresConfig = { ignores: merge(config.lint.exclude) };
+export const ignoresConfig = {
+  ignores: merge(config.exclude, config.lint.exclude),
+};
 export const rulesConfig = { rules: config.lint.rules };

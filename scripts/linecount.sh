606067e264d5f011c6bb5117b35466b790a1507d
Josh Holbrook <josh.holbrook@gmail.com>
2024-10-05 13:39:21 -0800

    Update linecount

--- scripts/linecount.sh
+++ scripts/linecount.sh
@@ -15,11 +15,13 @@ MATBAS_LC="$(cat ./*.ts \
 CITREE_LC="$(cat ./tools/citree/src/*.ts \
   ./tools/citree/src/templates/* | wc -l)"
 
+GRABTHAR_LC="$(cat ./tools/grabthar/*.mjs | wc -l)"
+
 TESTGEN_LC="$(cat ./tools/test-generator/*.ts | wc -l)"
 
 echo "project                 count"
 echo "--------               ------"
 echo "main                 ${MATBAS_LC}"
 echo "tools/citree         ${CITREE_LC}"
+echo "tools/grabthar       ${GRABTHAR_LC}"
 echo "tools/test-generator ${TESTGEN_LC}"
-

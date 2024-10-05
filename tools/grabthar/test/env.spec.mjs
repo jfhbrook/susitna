2fa368dc144de4cfcbae9c6e413050a18eb82f65
Josh Holbrook <josh.holbrook@gmail.com>
2024-10-05 15:15:01 -0800

    Test for parseBoolEnv in grabthar

---
+++ tools/grabthar/test/env.spec.mjs
@@ -0,0 +1,38 @@
+import { describe, expect, test } from 'vitest';
+
+import { parseBoolEnv } from '../util';
+
+const ENV_FALSE = [
+  undefined,
+  '',
+  '   ',
+  '0',
+  '  0  ',
+  'false',
+  'FALSE',
+  '  fAlSe  ',
+];
+
+const ENV_TRUE = [
+  '1',
+  '100',
+  '  1  ',
+  'true',
+  'TRUE',
+  '  tRuE  ',
+  'anything',
+];
+
+describe('parseBoolEnv', () => {
+  for (const value of ENV_FALSE) {
+    test(`the string \`${value}\` parses to \`false\``, () => {
+      expect(parseBoolEnv(value)).toBe(false);
+    });
+  }
+
+  for (const value of ENV_TRUE) {
+    test(`the string \`${value}\` parses to \`true\``, () => {
+      expect(parseBoolEnv(value)).toBe(true);
+    });
+  }
+});
---
+++ tools/grabthar/vitest.config.mjs
@@ -0,0 +1,36 @@
+import swc from 'unplugin-swc';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  test: {
+    exclude: ['node_modules']
+  },
+  plugins: [
+    swc.vite({
+  jsc: {
+    parser: {
+      syntax: 'typescript',
+      jsx: false,
+      decorators: true,
+      decoratorsBeforeExport: true,
+      dynamicImport: true,
+      privateMethod: true,
+      functionBind: true,
+      exportDefaultFrom: true,
+      exportNamespaceFrom: true,
+      topLevelAwait: false,
+      importMeta: false
+    },
+    transform: { legacyDecorator: false, decoratorMetadata: true },
+    target: 'es2022',
+    loose: false,
+    externalHelpers: false,
+    keepClassNames: true
+  },
+  module: { type: 'nodenext' },
+  sourceMaps: true,
+  inlineSourcesContent: true,
+  minify: false
+    }),
+  ],
+});

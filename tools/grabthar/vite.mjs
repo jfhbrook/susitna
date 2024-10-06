ca0ff7138ce34d94125847491c07b1864422616f
Josh Holbrook <josh.holbrook@gmail.com>
2024-10-05 18:51:35 -0800

    Push vite config into grabthar

---
+++ tools/grabthar/vite.mjs
@@ -0,0 +1,30 @@
+import { defineConfig as defineViteConfig } from 'vite';
+
+import swc from 'unplugin-swc';
+import { minify } from 'rollup-plugin-esbuild-minify';
+
+import config from './config.mjs';
+import { swcConfig } from './build.mjs';
+import { testConfig } from './test.mjs';
+
+export function defineConfig(cfg) {
+  return defineViteConfig(
+    Object.assign({}, cfg, {
+      build: {
+        ...(config.build || {}),
+        ssr: config.entrypoint,
+        outDir: config.outDir,
+      },
+      ssr: { ...(cfg.ssr || {}) },
+      test: {
+        ...(cfg.test || {}),
+        ...testConfig,
+      },
+      plugins: [
+        ...(cfg.plugins || []),
+        swc.vite(swcConfig),
+        config.build.minify && minify(),
+      ],
+    }),
+  );
+}
--- vite.config.mjs
+++ vite.config.mjs
@@ -1,59 +1,41 @@
 import process from 'node:process';
 
-import swc from 'unplugin-swc';
 import consts from 'rollup-plugin-consts';
 import jscc from 'rollup-plugin-jscc';
-import { minify } from 'rollup-plugin-esbuild-minify';
-import { defineConfig } from 'vite';
 import vitePkg from 'vite/package.json' with { type: 'json' };
 import swcPkg from '@swc/core/package.json' with { type: 'json' };
 
-import pkg from './package.json';
+import matbasPkg from './package.json';
 
-import {
-  config,
-  swcConfig,
-  testConfig,
-  parseBoolEnv,
-} from '@jfhbrook/grabthar';
+import { parseBoolEnv } from '@jfhbrook/grabthar';
+import { defineConfig } from '@jfhbrook/grabthar/vite';
 
 const MATBAS_BUILD =
   process.env.MATBAS_BUILD === 'release' ? 'release' : 'debug';
-const MATBAS_VERSION = pkg.version;
-
-const debugConfig = {
-  _MATBAS_BUILD: MATBAS_BUILD,
-  _DEBUG_TRACE: parseBoolEnv(process.env.DEBUG_TRACE),
-  _DEBUG_SHOW_TREE: parseBoolEnv(process.env.DEBUG_SHOW_TREE),
-  _DEBUG_TRACE_COMPILER: parseBoolEnv(process.env.DEBUG_TRACE_COMPILER),
-  _DEBUG_SHOW_CHUNK: parseBoolEnv(process.env.DEBUG_SHOW_CHUNK),
-  _DEBUG_TRACE_RUNTIME: parseBoolEnv(process.env.DEBUG_TRACE_RUNTIME),
-  _DEBUG_TRACE_GC: parseBoolEnv(process.env.DEBUG_TRACE_GC),
-};
 
 export default defineConfig({
-  build: {
-    ssr: config.entrypoint,
-    outDir: config.outDir,
-  },
-  ssr: {},
-  test: testConfig,
   plugins: [
     consts({
       matbas: {
         build: MATBAS_BUILD,
-        version: MATBAS_VERSION,
+        version: matbasPkg.version,
       },
       versions: {
-        matbas: MATBAS_VERSION,
+        matbas: matbasPkg.version,
         vite: vitePkg.version,
         swc: swcPkg.version,
       },
     }),
     jscc({
-      values: debugConfig,
+      values: {
+        _MATBAS_BUILD: MATBAS_BUILD,
+        _DEBUG_TRACE: parseBoolEnv(process.env.DEBUG_TRACE),
+        _DEBUG_SHOW_TREE: parseBoolEnv(process.env.DEBUG_SHOW_TREE),
+        _DEBUG_TRACE_COMPILER: parseBoolEnv(process.env.DEBUG_TRACE_COMPILER),
+        _DEBUG_SHOW_CHUNK: parseBoolEnv(process.env.DEBUG_SHOW_CHUNK),
+        _DEBUG_TRACE_RUNTIME: parseBoolEnv(process.env.DEBUG_TRACE_RUNTIME),
+        _DEBUG_TRACE_GC: parseBoolEnv(process.env.DEBUG_TRACE_GC),
+      },
     }),
-    swc.vite(swcConfig),
-    config.build.minify && minify(),
   ],
 });

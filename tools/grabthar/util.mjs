126bdb3635ee18eeef721ca2f6ab6942c9bd469c
Josh Holbrook <josh.holbrook@gmail.com>
2024-10-09 14:29:01 -0800

    Attempt to fix grabthar in node lts with polyfill

--- tools/grabthar/util.mjs
+++ tools/grabthar/util.mjs
@@ -1,8 +1,14 @@
+import { createRequire } from 'node:module';
+
 import { globSync } from 'glob';
+
+const MySet = Set.union
+  ? Set
+  : createRequire(import.meta.url)('es6-set/polyfill');
 
 export function merge(...lists) {
   return Array.from(
-    lists.reduce((acc, exc) => acc.union(new Set(exc)), new Set()),
+    lists.reduce((acc, exc) => acc.union(new MySet(exc)), new MySet()),
   ).sort();
 }
 

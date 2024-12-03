c65368cdf53fd6ffa99735deb2c3e5e55c53e4c0
Josh Holbrook <josh.holbrook@gmail.com>
2024-12-03 13:07:33 -0900

    Add tracing_subscriber::fmt

--- debug/crates/debug/src/lib.rs
+++ debug/crates/debug/src/lib.rs
@@ -1,4 +1,5 @@
 use neon::prelude::*;
+use tracing_subscriber;
 
 fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
     Ok(cx.string("hello node"))
@@ -6,6 +7,7 @@ fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
 
 #[neon::main]
 fn main(mut cx: ModuleContext) -> NeonResult<()> {
+    tracing_subscriber::fmt::init();
     cx.export_function("hello", hello)?;
     Ok(())
 }

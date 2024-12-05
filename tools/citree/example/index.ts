5c95efe41d40877612964f954f9fefa305226ed8
Josh Holbrook <josh.holbrook@gmail.com>
2024-12-05 07:42:11 -0900

    update citree tests

--- tools/citree/example/index.ts
+++ tools/citree/example/index.ts
@@ -1,3 +1,4 @@
+import { Source } from './source';
 import { Instr } from './instr';
 
 export interface TreeVisitor<R> {
@@ -15,7 +16,7 @@ export class Cmd extends Tree {
   constructor(
     public cmdNo: number,
     public row: number,
-    public source: string,
+    public source: Source,
     public instructions: Instr[],
   ) {
     super();
@@ -30,7 +31,7 @@ export class Line extends Tree {
   constructor(
     public lineNo: number,
     public row: number,
-    public source: string,
+    public source: Source,
     public instructions: Instr[],
   ) {
     super();
--- tools/citree/example/instr.ts
+++ tools/citree/example/instr.ts
@@ -1,6 +1,7 @@
 import { Expr, Variable } from './expr';
 
 export interface InstrVisitor<R> {
+  visitRemInstr(node: Rem): R;
   visitLetInstr(node: Let): R;
   visitAssignInstr(node: Assign): R;
   visitExpressionInstr(node: Expression): R;
@@ -13,7 +14,11 @@ export interface InstrVisitor<R> {
   visitRenumInstr(node: Renum): R;
   visitRunInstr(node: Run): R;
   visitSaveInstr(node: Save): R;
-  visitRemInstr(node: Rem): R;
+  visitShortIfInstr(node: ShortIf): R;
+  visitIfInstr(node: If): R;
+  visitElseInstr(node: Else): R;
+  visitElseIfInstr(node: ElseIf): R;
+  visitEndIfInstr(node: EndIf): R;
 }
 
 export abstract class Instr {
@@ -23,6 +28,20 @@ export abstract class Instr {
   ) {}
 
   abstract accept<R>(visitor: InstrVisitor<R>): R;
+}
+
+export class Rem extends Instr {
+  constructor(
+    public remark: string,
+    offsetStart: number = -1,
+    offsetEnd: number = -1,
+  ) {
+    super(offsetStart, offsetEnd);
+  }
+
+  accept<R>(visitor: InstrVisitor<R>): R {
+    return visitor.visitRemInstr(this);
+  }
 }
 
 export class Let extends Instr {
@@ -180,9 +199,11 @@ export class Save extends Instr {
   }
 }
 
-export class Rem extends Instr {
+export class ShortIf extends Instr {
   constructor(
-    public remark: string,
+    public condition: Expr,
+    public then: Instr[],
+    public else_: Instr[],
     offsetStart: number = -1,
     offsetEnd: number = -1,
   ) {
@@ -190,6 +211,54 @@ export class Rem extends Instr {
   }
 
   accept<R>(visitor: InstrVisitor<R>): R {
-    return visitor.visitRemInstr(this);
+    return visitor.visitShortIfInstr(this);
+  }
+}
+
+export class If extends Instr {
+  constructor(
+    public condition: Expr,
+    offsetStart: number = -1,
+    offsetEnd: number = -1,
+  ) {
+    super(offsetStart, offsetEnd);
+  }
+
+  accept<R>(visitor: InstrVisitor<R>): R {
+    return visitor.visitIfInstr(this);
+  }
+}
+
+export class Else extends Instr {
+  constructor(offsetStart: number = -1, offsetEnd: number = -1) {
+    super(offsetStart, offsetEnd);
+  }
+
+  accept<R>(visitor: InstrVisitor<R>): R {
+    return visitor.visitElseInstr(this);
+  }
+}
+
+export class ElseIf extends Instr {
+  constructor(
+    public condition: Expr,
+    offsetStart: number = -1,
+    offsetEnd: number = -1,
+  ) {
+    super(offsetStart, offsetEnd);
+  }
+
+  accept<R>(visitor: InstrVisitor<R>): R {
+    return visitor.visitElseIfInstr(this);
+  }
+}
+
+export class EndIf extends Instr {
+  constructor(offsetStart: number = -1, offsetEnd: number = -1) {
+    super(offsetStart, offsetEnd);
+  }
+
+  accept<R>(visitor: InstrVisitor<R>): R {
+    return visitor.visitEndIfInstr(this);
   }
 }

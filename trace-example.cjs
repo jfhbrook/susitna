84ffc42703bd5c7f22c4160d5e60289df2afceef
Josh Holbrook <josh.holbrook@gmail.com>
2024-12-09 19:22:57 -0900

    Apparently context fails if there isn't already a parent span?

--- trace-example.cjs
+++ trace-example.cjs
@@ -3,9 +3,14 @@ const { trace } = require('@opentelemetry/api');
 const tracer = trace.getTracer();
 
 async function main() {
-  await tracer.startActiveSpan('span', async (_) => {
-    console.log('inside span');
-  });
+  const span = tracer.startSpan('outer');
+  try {
+    await tracer.startActiveSpan('inner', async (_) => {
+      console.log('inside span');
+    });
+  } finally {
+    span.end();
+  }
 }
 
 main();
